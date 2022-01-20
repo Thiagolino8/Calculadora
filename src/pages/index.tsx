4505cdea1740947ef67d8e2786d3974bcfc010be
Thiago Lino Gomes <thiagolinog8@gmail.com>
2022-01-20 15:15:47 -0300

    Header fixed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,6 @@
 import { NextPage } from "next";
-import Head from "next/head";
 import Calculator from "../modules/calculator";
 
-const Home: NextPage = () => {
-  return (
-    <>
-      <Head>
-        <title>Calculadora</title>
-        <meta charSet="utf-8" />
-      </Head>
-      <Calculator />
-    </>
-  );
-};
+const Home: NextPage = () => <Calculator />;
 
 export default Home;
