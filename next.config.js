5153b273f04bf535fc7193dca0872e6a3e59afa8
Thiago Lino Gomes <thiagolinog8@gmail.com>
2022-03-28 18:31:40 -0300

    Added Preact

--- next.config.js
+++ next.config.js
@@ -14,6 +14,11 @@ module.exports = {
     formats: ['image/avif', 'image/webp']
   },
   webpack: (config, { isServer }) => {
+		Object.assign(config.resolve.alias, {
+			react: 'preact/compat',
+			'react-dom/test-utils': 'preact/test-utils',
+			'react-dom': 'preact/compat',
+		});
     if (!isServer) {
       // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
       config.resolve.fallback = {
